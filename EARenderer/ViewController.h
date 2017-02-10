d1297816d0b283ca791d18e2e14f7a757f5f9f26
Pavlo Muratov <pavlo.muratov@privatbank.ua>
2017-02-10 13:45:55 +0200

    Initial Commit

---
+++ EARenderer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EARenderer
+//
+//  Created by Pavlo Muratov on 10.02.17.
+//  Copyright © 2017 MPO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ EARenderer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EARenderer
+//
+//  Created by Pavlo Muratov on 10.02.17.
+//  Copyright © 2017 MPO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
